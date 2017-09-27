5603aeff681f770d82c79a1523be37530ad8c4c2
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-09-27 12:15:06 -0600

    I created a Method and Control file for practice

--- Methods and Controls.playground/Contents.swift
+++ Methods and Controls.playground/Contents.swift
@@ -6,3 +6,42 @@ public func isReallySimple() -> Void
 {
     print("I have had a really nice day today")
 }
+isReallySimple()
+
+public func aBitLessSimple(name: String) -> Void
+{
+    let answer = "my name is not " + name
+    print(answer)
+}
+aBitLessSimple(name: "slim shady")
+let words = "Marshall Mathers"
+aBitLessSimple(name: words)
+
+public func namingExample(outerName innerName :String) -> Void
+{
+    print("The inner name is: \(innerName)")
+}
+namingExample(outerName: "Alan")
+
+public func counting() -> Int
+{
+    return (7 * 665)
+}
+print("I am not \(counting()) years old")
+
+if(counting() < 10)
+{
+    print("Math works in swift too")
+}
+else
+{
+    print("Not very likely right now")
+}
+var test = 0
+while (test < counting())
+{
+    print("hahahahahah")
+    test += 1
+}
+
+print("YAY!!!!!")
