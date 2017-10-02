3c661f9e7d929a5ff9240b4e9b35a7f93382cdaf
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-10-02 12:25:02 -0600

    Added a class to my code

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var notesToday = "Using Swift to make classes and methods"
+
+publc class simpleClass
+{
+    private var name :String
+    
+    public init()
+    {
+        name = "My name is....."
+    }
+}
