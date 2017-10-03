a9b4064cdb6c5e0c0630e0f09ff51ba4ad41fb59
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-10-03 11:58:44 -0600

    added some changes to stupid project to practice swift

--- Stupid Project.playground/Contents.swift
+++ Stupid Project.playground/Contents.swift
@@ -5,9 +5,9 @@ import UIKit
 var myName :String
 var otherName = "words"
 var thisName :String = "Thor"
-print(thisName.count)
 print(thisName.characters.count)
 
-var age =2315
+var age = 2315
 var speed = 654.324536
 var rate = age/Int (speed)
+
